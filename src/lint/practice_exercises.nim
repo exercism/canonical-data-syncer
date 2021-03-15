ad66c81ec26bdc4ffb620f57a7e31a36ebf383d6
Erik Schierboom <erik_schierboom@hotmail.com>
2021-03-15 12:08:38 +0100

    lint(validators): extract required file checks to modules
    The validation that checked if required files were present was done in the `lint.nim` file. To better separate concerns, these file checks have been moved to their own modules.
    
    The validation helper proc to check for required files has been moved to the validators module.

--- src/lint/practice_exercises.nim
+++ src/lint/practice_exercises.nim
@@ -50,3 +50,15 @@ proc isEveryPracticeExerciseConfigValid*(trackDir: string): bool =
             continue
         if not isValidPracticeExerciseConfig(j, configPath):
           result = false
+
+proc practiceExerciseFilesExist*(trackDir: string): bool =
+  ## Returns true if every subdirectory in `trackDir/exercises/practice` has the
+  ## required files.
+  const
+    requiredPracticeExerciseFiles = [
+      ".docs" / "instructions.md",
+      ".meta" / "config.json",
+    ]
+
+  let practiceExercisesDir = trackDir / "exercises" / "practice"
+  result = subdirsContain(practiceExercisesDir, requiredPracticeExerciseFiles)
